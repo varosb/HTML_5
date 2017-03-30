b6240bf718679b01d222849ceb4ed9567f2fa378
alvarosb <alvarosanchez.ciclos@gmail.com>
2017-03-30 12:04:24 +0200

    Actualizo

--- Ejercicio 03 - Series/V2/scripts/scriptSeries.js
+++ Ejercicio 03 - Series/V2/scripts/scriptSeries.js
@@ -1,14 +1,19 @@
-function Acierto1_2() 
+function Aciertos(idRespuesta,idImagen)
 {
-	var p1  /* Parrafo de respuesta */
-	   ,p2; /* Parrafo de resultado */
+	var p1,
+		p2,
+		imagen;
 
-	p1 = document.getElementById('Respuesta1_2');
+	p1 = document.getElementById(idRespuesta);
 	p1.style.color = "green";
-	imagen = document.getElementById('Imagen1');
-	imagen.src = "imagenes/acierto.png";
+	imagen = document.getElementById(idImagen),
+	imagen.serc = "imagenes/acierto.png";
 	p2 = document.getElementById('Resultados');
-	p2.innerHTML += "1";
+	p2.innerHTML += "1";	
+}
+
+function Errores()
+
 }
 function Error1_1() 
 {
@@ -34,17 +39,7 @@ function Error1_3()
 	p2 = document.getElementById('Resultados');
 	p2.innerHTML += "0";
 }
-function Acierto2_3() 
-{
-	var p1  /* Parrafo de respuesta */
-	   ,p2; /* Parrafo de resultado */
 
-	p1 = document.getElementById('Respuesta2_3');
-	p1.style.color = "green";
-	imagen = document.getElementById('Imagen1');
-	imagen.src = "imagenes/acierto.png";
-	p2 = document.getElementById('Resultados');
-	p2.innerHTML += "1";
 }
 function Error2_1() 
 {
